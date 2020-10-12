11df548c91dfe70eae4ffb222272873ad8385669
enxiety <66563082+enxiety@users.noreply.github.com>
2020-10-12 12:40:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,23 +4,16 @@ import string
 chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=+_/';.,[]`"
 chars_list = list(chars)
 password = input("Enter a password: ")
-guesses_used = 0
+tries = 0
 guess_password = ""
 
 while guess_password != password:
     guess_password = random.choices(chars_list, k=len(password))
-    guesses_used += float(1)
+    tries += float(1)
 
-    print(guess_password, guesses_used, chars.isupper(), )
-
-    if chars.isupper():
-        guesses_used = + str(2)
+    print(guess_password, tries, "<- number of tries")
 
     if guess_password == list(password):
         print("Your password is: " + "".join(guess_password))
-        input("Press enter to exit")
+        input("Press enter to exit ")
         break
-
-    if guesses_used == 10000000:
-        print("Too many tries, quitting program")
-        exit(0)
