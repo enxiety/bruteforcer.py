8937c7b888917ddbc5eda0ca70f956ad8e288c85
enxiety <66563082+enxiety@users.noreply.github.com>
2020-10-12 09:13:42 -0400

    Update main.py
    Added more chars, special chars, and added a guess limit to prevent performance issues, also some improved math

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import random
 import string
 
-chars = "abcdefghijklmnopqrstuvwxyz0123456789"
+chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-=+_/';.,[]`"
 chars_list = list(chars)
 password = input("Enter a password: ")
 guesses_used = 0
@@ -11,9 +11,16 @@ while guess_password != password:
     guess_password = random.choices(chars_list, k=len(password))
     guesses_used += float(1)
 
-    print(guess_password, guesses_used)
+    print(guess_password, guesses_used, chars.isupper(), )
+
+    if chars.isupper():
+        guesses_used = + str(2)
 
     if guess_password == list(password):
-        print("Your password is : " + "".join(guess_password))
+        print("Your password is: " + "".join(guess_password))
         input("Press enter to exit")
         break
+
+    if guesses_used == 10000000:
+        print("Too many tries, quitting program")
+        exit(0)
